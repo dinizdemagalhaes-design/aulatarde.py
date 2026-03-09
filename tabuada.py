ce0621e0439c6e29926ab59bbf9f41cee7cc44f3
dinizdemagalhaes-design <dinizdemagalhaes@icloud.com>
2026-03-09 15:41:30 -0300

    Add multiplication table for user-defined number

--- tabuada.py
+++ tabuada.py
@@ -1,3 +1,13 @@
 for i in range(1,11):
     resultado = 3 * i
     print(f" 3 x 1{i} = {resultado}")
+
+
+
+outro modo
+
+tabu = int(input("Digite um numero 0 a 10: "))
+
+for i in range(1,11):
+    resultado = tabu * i
+    print(f" tabu x {i} = {resultado}")
