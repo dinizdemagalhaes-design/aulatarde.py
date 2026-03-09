a5d6895882d5376d692d57471796749a2c694b43
dinizdemagalhaes-design <dinizdemagalhaes@icloud.com>
2026-03-09 17:22:21 -0300

    Implement approval check for student grades
    Added logic to print approval status based on grades.

---
+++ listaaprovado.py
@@ -0,0 +1,15 @@
+my_list = [5.5, 7.0, 8.2, 6.0, 9.1]
+for list in my_list:
+    if list >= 7:
+        print ("Aluno aprovado com media é:", list)
+
+
+
+outro caso:
+
+my_list = [5.5, 7.0, 8.2, 6.0, 9.1]
+for list in my_list:
+    if list >= 7:
+        print ("Aluno aprovado com media é:", list)
+    else:
+        print ("Em recuperação é ", list)    
