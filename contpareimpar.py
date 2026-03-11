68a82b2b71448624714ffb13682f1b1df9e80d69
dinizdemagalhaes-design <dinizdemagalhaes@icloud.com>
2026-03-11 15:42:36 -0300

    Add script to count even and odd numbers

---
+++ contpareimpar.py
@@ -0,0 +1,14 @@
+numeros = (5, 8, 0, 12, 7, 3, 10, 4, 9, 6)
+pares = 0
+impares = 0
+
+for numero in numeros:
+    if numero == 0:
+        print ("Encontrado o número zero")
+    elif numero %2 ==0 and numero != 0:
+        pares += 1
+    else:
+        impares += 1
+    
+print ("A quantidade de pares é", pares )
+print ("A quantidade de ímpares é", impares)
