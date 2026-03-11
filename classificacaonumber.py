2d81f8a29aa1c032167948a3bc8ff489dd8afc80
dinizdemagalhaes-design <dinizdemagalhaes@icloud.com>
2026-03-11 14:47:30 -0300

    Add classification logic for positive, negative, and zero

---
+++ classificacaonumber.py
@@ -0,0 +1,11 @@
+
+
+
+for i in range(5):
+    numero = float(input("Digite o numero: "))
+    if numero > 0:
+        print ("Número positio")
+    elif numero < 0:
+        print ("Númeor negativo")
+    else :
+        print ("O número é zero")
