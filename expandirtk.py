3c576b9a6305a0a008005f410f7ec50291d598f7
dinizdemagalhaes-design <dinizdemagalhaes@icloud.com>
2026-03-17 15:31:25 -0300

    Update expandirtk.py

--- expandirtk.py
+++ expandirtk.py
@@ -1,42 +1,34 @@
 import tkinter as tk
 
+janela = tk.Tk()
+janela.title("Estrutura base")
+janela.geometry("600x400")
 
-'''Estrutura do projeto'''
-root = tk.Tk()
-root.title("Esstrutura base")
-root.geometry("600x400")
+# Configuração da grade da janela
+janela.rowconfigure(0, weight=1)
+janela.rowconfigure(1, weight=1)
+janela.columnconfigure(0, weight=1)
+janela.columnconfigure(1, weight=3)
 
-'''Linhas da janela para aumentar'''
-root.rowconfigure(0, weight=1)
-root.rowconfigure(1, weight=1)
-
-'''Colunas da janela para aumentar'''
-root.columnconfigure(0,weight=1)
-root.columnconfigure(1,weight=3)
-
-'''Criar Frame esquerdo'''
-frame_esquerdo = tk.Frame(root, bg="ligthblue")
+# Frame esquerdo (W)
+frame_esquerdo = tk.Frame(janela, bg="lightblue")
 frame_esquerdo.grid(row=0, column=0, rowspan=2, sticky="nsew")
 
-'''Criar texto esquerdo'''
-label_esquerdo = tk.Label(frame_esquerdo, text="Frame Esquerdo")
-label_esquerdo.pack(expand=True)
+label_esq = tk.Label(frame_esquerdo, text="Frame Esquerdo")
+label_esq.pack(expand=True)
 
-'''Criar frame direito'''
-frame_centro = tk.Frame(root, bg="lightgreen")
-frame_centro.grid(row=0,column=1, sticky="nsew")
+# Frame superior direito
+frame_centro = tk.Frame(janela, bg="lightgreen")
+frame_centro.grid(row=0, column=1, sticky="nsew")
 
-'''Criar texto do frame direito'''
-label_centro = tk.Label(frame_centro, text= "Frame direito")
+label_centro = tk.Label(frame_centro, text="Frame direito")
 label_centro.pack(expand=True)
 
-'''Criar frame inferior direito'''
-frame_inferior = tk.Frame(root, bg="yelow")
+# Frame inferior
+frame_inferior = tk.Frame(janela, bg="yellow")
 frame_inferior.grid(row=1, column=1, sticky="nsew")
 
-'''Texto inferior direito'''
-label_inferior = tk.Label(frame_inferior, text="Frame Inferior")
-label_inferior.pack(expand=True)
+label_inf = tk.Label(frame_inferior, text="Frame Inferior Direito")
+label_inf.pack(expand=True)
 
-
-root.mainloop
+janela.mainloop()
