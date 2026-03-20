aef4f744f852ae37efc0f4ce17c54d1fa152f941
dinizdemagalhaes-design <dinizdemagalhaes@icloud.com>
2026-03-20 17:22:19 -0300

    Create exerciciolista.py

---
+++ exerciciolista.py
@@ -0,0 +1,43 @@
+lista = []
+
+while True:
+    print("\n====== Lista de Compras======")
+    print("[i] Inserir um item")
+    print("[a] Apagar item")
+    print ("[l] Listar item")
+
+    opção = input("Escolha uma opção: ").lower()
+   
+    if opção == "i":
+        item = input("Digite o item:")
+        lista.append(item)
+        print("Item adicionado com sucesso")  
+
+    elif opção=="a":
+        try:
+            if not lista:
+                print("A lista está vazia.")
+            else:
+                for i, item in enumerate(lista):
+                    print(i, "-", item)
+                
+                indice =int(input("Digite o índice do item: "))
+                removido = lista.pop(indice)
+                print(f"item '{removido} removido!")
+        
+        except ValueError:
+            print("Digite um numero invalido")
+        except IndexError:
+            print("Indice nao existe")
+
+    elif opção == "l":
+        if not lista:
+            print(("A lista esta vazia"))
+        
+        else:
+            print("Itens da lista:")
+            for i, item in enumerate(lista):
+                print(i, "-", item)
+    
+    else:
+        print("Opção inválida")
